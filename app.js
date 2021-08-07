d3fd4e579dedd64e86732cdaae85aac59377f60e
victorsouza19 <victor.cristofalo@hotmail.com>
2021-08-07 17:55:34 -0300

    db/server/dotenv finished, making hbs

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
-const express = require("express");
+// Importing the dependencies | Importando as dependências
+    const express = require("express");
+    const path = require('path');
+    const mysql = require("mysql");
+    const dotenv = require('dotenv');
 
-const app = express();
 
-app.get("/", (req, res) => {
-    res.send("<h1> Home Page</h1>")
-});
+// changing where the environment variables will be stored by dotenv | definindo onde ficarão armazenadas as variáveis de ambiente pelo dotenv 
+    dotenv.config({ path: './.env'});
 
-app.listen(3000, () => {
-    console.log("Server started on Port 3000");
-})
+// starting express | iniciando o express
+    const app = express();
+
+// making the connection to database | Fazendo a conexão com o banco de dados
+    const db = mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE
+    });
+
+// setting the publicDirectory | configurando o diretório public
+    const publicDirectory = path.join(__dirname, './public');
+// setting where the express will search the public files || definindo onde o express buscará os arquivos públicos
+    app.use(express.static(publicDirectory));
+
+// setting the view engine | definindo o mecanismo de visualização
+    app.set('view engine', 'hbs');
+
+// verifying the connection to the db | verificando a conexão com o bd
+    db.connect( (error) => {
+        if(error) {
+            console.log(error)
+        } else {
+            console.log("MYSQL Connected...")
+        }
+    });
+
+
+    app.get("/", (req, res) => {
+        // only for test: res.send("<h1> Home Page</h1>")
+
+        // Setting the homepage | Definindo a homepage
+        res.render("index"); 
+    });
+
+
+// verifying the connection to the server | verificando a conexão com o servidor
+    app.listen(3000, () => {
+        console.log("Server started on Port 3000");
+    })
