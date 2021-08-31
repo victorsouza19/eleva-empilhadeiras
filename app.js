886eb5dfb79a27525ceed0f505df9cd3c2d9660b
victorsouza19 <victor.cristofalo@hotmail.com>
2021-08-30 21:41:06 -0300

    azure db connected

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: process.env.DATABASE_PORT,
+
 });
 
 module.exports = db;
